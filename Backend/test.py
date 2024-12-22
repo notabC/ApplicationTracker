6d3780cad4a5ad74f0397d35f68857e393b2465f
notabC <70454573+notabC@users.noreply.github.com>
2024-12-22 17:17:49 +0000

    Delete test.py

--- Backend/test.py
+++
@@ -1,9 +0,0 @@
-import smtplib
-
-EMAIL_ADDRESS = "donotreply@trackwise.pro"
-EMAIL_PASSWORD = "2TipztBDRM5j"
-
-with smtplib.SMTP("smtp.zoho.eu", 587) as server:
-    server.starttls()
-    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    print("Login successful!")
